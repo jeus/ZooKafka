f3c8f1c78f3a3b41348f6be62dd92901db833704
jeus <jeus@jeus-datis>
2016-06-18 12:20:40 +0430

    first commit

---
+++ src/main/java/com/datis/zookafka/client/test1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.datis.zookafka.client;
+
+/**
+ *
+ * @author jeus
+ */
+public class test1 {
+
+    public test1()
+    {
+    }
+    
+    private void newZk()
+    {
+        
+    }
+    
+    
+    public static void main(String args[]) throws Exception {
+        
+        
+        
+    }
+}
